c42a1f4d22861971d09dab12bf7226ceb29227c0
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2024-05-11 16:34:11 +0700

    Update Main, Home Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,44 @@
     <!-- Masthead-->
     <header class="masthead bg-primary text-white text-center">
         <div class="container d-flex align-items-center flex-column">
-            <!-- Masthead Avatar Image-->
-            <img class="masthead-avatar mb-5" src="assets/assets/img/avataaars.svg" alt="..." />
+
+
+            {{-- <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="assets/assets/img/avataaars.svg" alt="..." /> --}}
+
+
             <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
-            <!-- Icon Divider-->
+            <h1 class="masthead-heading text-uppercase mb-0">Astronomia</h1>
+
+            {{-- <!-- Icon Divider-->
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
                 <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                 <div class="divider-custom-line"></div>
             </div>
+
             <!-- Masthead Subheading-->
-            <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+            <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p> --}}
+        </div>
+    </header>
+    <header class="masthead bg-primary text-white text-center bg-black">
+        <div class="container d-flex align-items-center flex-column">
+
+            {{-- <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="assets/assets/img/avataaars.svg" alt="..." /> --}}
+
+            <!-- Masthead Heading-->
+            <h1 class="masthead-heading text-uppercase mb-0">Tentang Kami</h1>
+
+            {{-- <!-- Icon Divider-->
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div> --}}
+
+            {{-- <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p> --}}
         </div>
     </header>
     <!-- Portfolio Section-->
@@ -82,6 +108,24 @@
                     </div>
                 </div>
                 <!-- Portfolio Item 6-->
+                <div class="col-md-6 col-lg-4">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
+                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div class="portfolio-item-caption-content text-center text-white"><i
+                                    class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/assets/img/portfolio/submarine.png" alt="..." />
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
+                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div class="portfolio-item-caption-content text-center text-white"><i
+                                    class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/assets/img/portfolio/submarine.png" alt="..." />
+                    </div>
+                </div>
                 <div class="col-md-6 col-lg-4">
                     <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
                         <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 

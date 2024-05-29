b658dc9ee5d5db89af9418e5c27e395487274ae4
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2024-05-29 21:53:54 +0700

    memperbaiki main blade

--- resources/views/kakastro/kakastro.blade.php
+++ resources/views/kakastro/kakastro.blade.php
@@ -2,116 +2,90 @@
 
 @section('container1')
 
-    <section class="masthead bg-primary text-white text-center" style="background-image: url('assets/assets/img/universe.jpg');
+<section class="masthead bg-primary text-white text-center" style="background-image: url('assets/assets/img/universe.jpg');
     height:cover; overflow:auto; background-repeat: no-repeat;
   background-attachment: fixed;">
-        <div class="container d-flex align-items-center flex-column">
-            <!-- Masthead Avatar Image-->
-            {{-- <img class="masthead-avatar mb-5" src="assets/assets/img/avataaars.svg" alt="..." /> --}}
-            <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">kak Astro</h1>
+    <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+        {{-- <img class="masthead-avatar mb-5" src="assets/assets/img/avataaars.svg" alt="..." /> --}}
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">kak Astro</h1>
 
-            <br>
-            <!-- Icon Divider-->
-           
-
-            
+        <br>
+        <!-- Icon Divider-->
 
 
-            {{-- <!-- Masthead Subheading-->
+
+
+
+        {{-- <!-- Masthead Subheading-->
             <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p> --}}
-        </div>
-    </section>
+    </div>
+</section>
 
-    
 
-    <div class="divider-custom divider-light">
-                <div class="divider-custom-line"></div>
-                <!-- <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+
+<div class="divider-custom divider-light">
+    <div class="divider-custom-line"></div>
+    <!-- <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                 <div class="divider-custom-line"></div> -->
-            </div>
+</div>
 
 
-    <!-- Portfolio Section-->
-    <section class="page-section portfolio" id="portfolio">
-        <div class="container">
-            <!-- Portfolio Section Heading-->
-            <!-- <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Glosarium</h2> -->
-            
-            <!-- Icon Divider-->
-            <!-- <div class="divider-custom divider-light">
+<!-- Portfolio Section-->
+<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <!-- Portfolio Section Heading-->
+        <!-- <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Glosarium</h2> -->
+
+        <!-- Icon Divider-->
+        <!-- <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
                 <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                 <div class="divider-custom-line"></div>
             </div> -->
-            
-            <!-- Portfolio Grid Items-->
-            <div class="row justify-content-center">
-                <!-- Portfolio Item 1-->
-                <div class="col-md-6 col-lg-4 mb-5">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/cabin.png" alt="..." />
-                    </div>
-                </div>
-                <!-- Portfolio Item 2-->
-                <div class="col-md-6 col-lg-4 mb-5">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/cake.png" alt="..." />
-                    </div>
-                </div>
-                <!-- Portfolio Item 3-->
-                <div class="col-md-6 col-lg-4 mb-5">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/circus.png" alt="..." />
-                    </div>
-                </div>
-                <!-- Portfolio Item 4-->
-                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/game.png" alt="..." />
-                    </div>
-                </div>
-                <!-- Portfolio Item 5-->
-                <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/safe.png" alt="..." />
-                    </div>
-                </div>
-                <!-- Portfolio Item 6-->
-                <div class="col-md-6 col-lg-4">
-                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
-                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="assets/assets/img/portfolio/submarine.png" alt="..." />
-                    </div>
-                </div>
 
-
+        <!-- Portfolio Grid Items-->
+        <div class="row justify-content-center">
+            <!-- Portfolio Item 1-->
+            <div class="col-md-5 col-lg-3 mb-4">
+                <div class="portfolio-item mx-auto border border-light" data-bs-toggle="modal"
+                    data-bs-target="#portfolioModal1">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white">
+                            <i>Mengapa Gerhana Matahari dan Bulan Bisa Terjadi?</i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/assets/img/gerhanaM.jpeg" alt="..." />
+                </div>
             </div>
+            <!-- Portfolio Item 2-->
+            <div class="col-md-5 col-lg-3 mb-4">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white">
+                            <i>Mengapa Bumi Bisa Mengalami musim?</i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/assets/img/orbitbumi2.jpeg" alt="..." />
+                </div>
+            </div>
+            <!-- Portfolio Item 3-->
+            <div class="col-md-5 col-lg-3 mb-4">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white">
+                            <i>Mengapa Venus Tidak Memiliki Satelit Alami Seperti Bumi?</i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/assets/img/venus2.jpeg" alt="..." />
+                </div>
+            </div>
+
+
         </div>
-    </section>
+    </div>
+</section>
 
 
 

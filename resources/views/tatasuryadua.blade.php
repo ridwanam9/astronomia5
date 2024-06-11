59a94f37239d927a741fddaec312222f78cc5746
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2024-06-11 19:54:05 +0700

    update navbar for tatasuryadua

--- resources/views/tatasuryadua.blade.php
+++ resources/views/tatasuryadua.blade.php
@@ -18,7 +18,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                        href="/tatasurya">Jelajah</a></li>
+                        href="/tatasuryadua">Jelajah</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
                         href="/glosarium">Glosarium</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/kakastro">Kak

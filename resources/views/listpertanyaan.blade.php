271f927318939c45e46f0d133de71ee0f9f0101c
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2024-06-18 22:41:29 +0700

    make table in listpertanyaan

--- resources/views/listpertanyaan.blade.php
+++ resources/views/listpertanyaan.blade.php
@@ -3,7 +3,7 @@
 @section('container1')
 <!-- Masthead-->
 <header class="masthead text-white text-center" style="background-image: url('assets/assets/img/universe.jpg');
-    height:cover; overflow:auto; background-repeat: no-repeat;
+    height:100vh; overflow:auto; background-repeat: no-repeat;
   background-attachment: fixed;">
     <div class="container d-flex align-items-center flex-column">
 
@@ -30,57 +30,55 @@
         </div>
         @endif
         <br>
+        <div class="container">
+            <div class="row justify-content-center mb-3">
+                <div class="col-md-6">
+                    <form action="{{ route('tanya.index') }}" method="GET">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-primary" type="submit">Search</button>
+                            </div>
+                            <input type="text" name="search" placeholder="Cari..." class="form-control" aria-label=""
+                                aria-describedby="basic-addon1" value="{{ session('last_search') }}">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
         <!-- Masthead Subheading-->
         <!-- <p class="masthead-subheading font-weight-light mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi reiciendis assumenda, excepturi, recusandae quibusdam natus voluptatum nemo necessitatibus fugiat dicta ratione quas illum soluta. Quae, eligendi cupiditate consequatur ipsam unde ducimus iusto tempore cum sint perferendis! Cupiditate, quam dolores? Omnis accusamus et error. Est eaque assumenda porro cupiditate et! Eligendi voluptas nihil omnis ab? Blanditiis velit perspiciatis assumenda dolorem voluptatem! In adipisci nostrum possimus laudantium totam tenetur provident veniam minus officia. Iste iusto in voluptatibus dolor? Quae nam fuga ipsum nesciunt tenetur reiciendis, eum incidunt voluptas earum praesentium, hic perferendis quasi pariatur odio repellat exercitationem aperiam sit. Aliquam, iusto mollitia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur architecto officia harum asperiores alias, molestiae aperiam quibusdam nemo dignissimos omnis! Quas saepe officia voluptates itaque esse numquam aut recusandae impedit voluptate aliquam excepturi illo, debitis reprehenderit similique expedita deleniti earum totam cumque facilis enim natus molestias amet sequi. Voluptatum enim vel, molestias, expedita, dolor mollitia quae maxime maiores adipisci non possimus praesentium est? Ratione quisquam iste at itaque, rerum excepturi ullam autem aut fuga, delectus vel in cupiditate inventore accusamus eligendi nobis recusandae tempora asperiores deserunt veritatis sint vero maxime facilis iure? Ducimus ab mollitia dolor, dicta nostrum neque exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maxime eum iure magnam tenetur quo alias, expedita sequi beatae quidem reprehenderit labore cupiditate nostrum tempora perferendis ducimus obcaecati. Quod harum possimus officia nam, porro consequuntur necessitatibus quae repudiandae repellendus obcaecati et beatae eum autem ex repellat debitis aliquid cum aliquam dolorum blanditiis ut? Beatae unde eveniet natus rerum vitae inventore, exercitationem soluta vero atque laborum dolore maxime iste quibusdam iusto animi quidem laboriosam at necessitatibus, nesciunt suscipit. Facere doloremque velit mollitia veritatis possimus labore, unde numquam! Vitae quibusdam voluptate totam, culpa cum commodi explicabo odit nesciunt facilis suscipit iure temporibus?</p> -->
+        <div class="container mt-5">
+            <div class="row">
+                <table class="table bg-secondary rounded" style="color: white;">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Pertanyaan</th>
+                            <th scope="col">Nama Pengirim</th>
+                            <th scope="col">Email Pengirim</th>
+                            <th scope="col">No Handphone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($tanyas as $tanya)
+                        @php $loopIndex = $loop->index + 1; @endphp
+                        <tr class="px-6 py-4 font-medium text-gray-900 md:whitespace-nowrap dark:text-white">
+                            <th scope="row">{{ $loopIndex }}</th>
+                            <td>{{ $tanya->judul }}</td>
+                            <td>{{ $tanya->user->name }}</td>
+                            <td>{{ $tanya->user->email }}</td>
+                            <td>{{ $tanya->user->handphone }}</td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="6" style="text-align: center;">Kosong</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </header>
 
-<div class="container">
-    <div class="row justify-content-center mb-3">
-        <div class="col-md-6">
-            <form action="{{ route('tanya.index') }}" method="GET">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <button class="btn btn-primary" type="submit">Search</button>
-                    </div>
-                    <input type="text" name="search" placeholder="Cari..." class="form-control" aria-label=""
-                        aria-describedby="basic-addon1" value="{{ session('last_search') }}">
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<section class="page-section" style="
-    height: cover; overflow: auto">
-<div class="container mt-5">
-        <div class="row">
-            @foreach($tanyas as $tanya)
-                <div class="col-md-4">
-                
-                    <div class="card mb-4 bg-primary text-white">
-                    <form action="{{ route('tanyas.destroy', $tanya->id) }}" method="POST"
-                                style="display: inline-block;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-warning btn-sm mt-3"
-                                    onclick="return confirm('Are you sure you want to delete this item?')">
-                                    <i class="bi bi-x"></i> Delete
-                                </button>
-                            </form>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $tanya->judul }}</h5>
-                            <p class="card-text">Penanya : {{ $tanya->user->name }}</p>
-                            <p class="card-text">Email : {{ $tanya->user->email }}</p>
-                            <p class="card-text">No Handphone : {{ $tanya->user->handphone }}</p>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</section>
-
-
 @endsection
-

760bf785b4cf648a1d23449a1ae1d2a00f827653
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2024-07-01 11:42:01 +0700

    update middleware daftartanya blade

--- resources/views/user/daftartanya.blade.php
+++ resources/views/user/daftartanya.blade.php
@@ -83,10 +83,12 @@
                                             Delete
                                         </button>
                                     </form>
+                                    @if(!$tanya->kakastro)
                                     <a href="{{ route('tanya.edit', $tanya->id) }}"
                                         class="text-red-600 dark:text-red-400 whitespace-nowrap">
                                         Update
                                     </a>
+                                    @endif
                                 </div>
                             </td>
                             <td>
